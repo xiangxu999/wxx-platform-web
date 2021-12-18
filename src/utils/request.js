f6a05cfec96e1f3cd8deae4856b22c876d0772ac
文享旭 <846212939@qq.com>
2021-12-18 11:26:37 +0800

    请求更新

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
-import { MessageBox, Message } from 'element-ui'
+import { Message } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
+import Config from '@/settings'
+import Cookies from 'js-cookie'
+import router from '@/router'
 
 // 创建一个axios实例
 const service = axios.create({
@@ -9,7 +12,7 @@ const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   // send cookies when cross-domain requests
   // withCredentials: true,
-  timeout: 5000 // request timeout
+  timeout: Config.timeout // request timeout
 })
 
 // request interceptor
@@ -17,7 +20,7 @@ service.interceptors.request.use(
   config => {
     // do something before request is sent
 
-    if (store.getters.token) {
+    if (getToken()) {
       // let each request carry token
       // ['Authorization'] is a custom headers key
       // please modify it according to the actual situation
@@ -46,39 +49,49 @@ service.interceptors.response.use(
    */
   response => {
     const res = response.data
-    if (res.code !== 200) {
+    if (res.code) {
+      if (res.code === 401) { // 401: 没有登录
+        // to re-login
+        store.dispatch('user/resetToken').then(() => {
+          // 用户登录界面提示
+          Cookies.set('point', 401)
+          location.reload()
+        })
+      } else if (res.code === 403) { // 403: 没有权限
+        router.push({ path: '/401' })
+      } else if (res.code === 500 || res.code === 501) { // 500: 操作失败 501: 校验失败
+        Message({
+          message: res.message || 'Error',
+          type: 'error',
+          duration: 5 * 1000
+        })
+      } else if (res.code === 200) { // 200: 操作成功
+        return res
+      }
+    } else {
       Message({
-        message: res.message || 'Error',
+        message: '接口请求失败',
         type: 'error',
         duration: 5 * 1000
       })
-
-      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      // 401: UnAuthorized
-      if (res.code === 401) {
-        // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
-            location.reload()
-          })
-        })
-      }
-      return Promise.reject(new Error(res.message || 'Error'))
-    } else {
-      return res
     }
+    return Promise.reject(new Error(res.message || 'Error'))
   },
   error => {
-    console.log(error)
-    console.log('err' + error) // for debug
+    // 网络超时
+    if (error.toString().indexOf('Error: timeout') !== -1) {
+      Message({
+        message: '网络请求超时',
+        type: 'error',
+        duration: 5000
+      })
+      return Promise.reject(error)
+    }
+    // 其他异常错误
     Message({
       message: error.response.data !== null ? error.response.data.message : error.message,
       type: 'error',
-      duration: 5 * 1000
+      duration: 5000
     })
     return Promise.reject(error)
   }
