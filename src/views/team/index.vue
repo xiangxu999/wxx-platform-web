1e8be53db805927c07f21f08e173174a0cf40143
文享旭 <846212939@qq.com>
2021-12-23 15:24:34 +0800

    团队信息更新

--- src/views/team/index.vue
+++ src/views/team/index.vue
@@ -2,7 +2,7 @@
   <div class="card-container">
     <el-row :gutter="20">
       <el-col v-for="item of blogList" :key="item.qq" :span="6">
-        <info-card class="item" :img="item.img" :username="item.username" :nickname="item.nickname" :qq="item.qq" :email="item.email" :blog="item.blog" />
+        <info-card class="item" :img="item.img" :username="item.username" :nickname="item.nickname" :birthday="item.birthday" :qq="item.qq" :email="item.email" :blog="item.blog" />
       </el-col>
     </el-row>
   </div>
